c8b1770283999173ae193cc161f5cc788980aacc
Kuba <ut_3612@zsp3zamosc.pl>
2024-03-04 15:35:00 +0100

    v2

--- SQLite/MainPage.xaml.cs
+++ SQLite/MainPage.xaml.cs
@@ -8,6 +8,12 @@ namespace SQLite
         public MainPage()
         {
             InitializeComponent();
+
+
+
+
+
+            int nr = 1;
             conn = new SQLiteConnection(connStr);
             conn.Open();
             string q = "SELECT * FROM pojazd";
@@ -15,18 +21,57 @@ namespace SQLite
             SQLiteCommand command = conn.CreateCommand();
             command.CommandText = q;
             SQLiteDataReader rdr = command.ExecuteReader();
+            rdr.Read();
+            wierszeLbl.Text += (rdr[0].ToString() + ". "+
+                                rdr[1].ToString() + " "+
+                                rdr[2].ToString() + " "+
+                                rdr[3].ToString() + " "+
+                                rdr[4].ToString() + "r "+
+                                rdr[5].ToString() + "km "+
+                                rdr[6].ToString() + "zł ");
+            pojazdImg.Source = (rdr[7].ToString()+".png");
 
-
+            /*
             while (rdr.Read())
             {
                 for (var i = 0; i < rdr.FieldCount-1; i++)
                     wierszeLbl.Text+=(rdr[i].ToString() + " ");
                 wierszeLbl.Text += "\n";
             }
+            */
             conn.Close();
 
 
         }
+
+        private void OnPoprzedniClicked(object sender, EventArgs e)
+        {
+            nr++;
+            wierszeLbl.Text += (rdr[0].ToString() + ". " +
+                    rdr[1].ToString() + " " +
+                    rdr[2].ToString() + " " +
+                    rdr[3].ToString() + " " +
+                    rdr[4].ToString() + "r " +
+                    rdr[5].ToString() + "km " +
+                    rdr[6].ToString() + "zł ");
+            pojazdImg.Source = (rdr[7].ToString() + ".png");
+        }
+
+        private void OnNastepnyClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnWybranyClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnUsunClicked(object sender, EventArgs e)
+        {
+
+        }
+
 
     }
 }
